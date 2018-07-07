de56e79af20cc2f317585b972d3f335bf3b47885
dhavalcbarot04 <40911909+dhavalcbarot04@users.noreply.github.com>
2018-07-07 02:55:49 -0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'Age'=> \Illuminate\Routing\Middleware\AgeMiddleware::class,
+        'Role'=> \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
